03b4a801da058676abad2e0fb1260e96314f3d00
Eduardo <eduardojunior010757@gmail.com>
2024-12-04 22:01:45 -0300

    refact

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
+import shutil
 import cv2
 import numpy as np
-from sklearn.cluster import DBSCAN
+from sklearn.cluster import KMeans
 from keras_facenet import FaceNet
 
 def preprocess_image(img_path: str) -> np.array:
@@ -12,8 +13,7 @@ def preprocess_image(img_path: str) -> np.array:
         
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img = cv2.resize(img, (160, 160))
-        normalized_img = img / 255.0
-        return normalized_img
+        return img
     
     except Exception as e:
         print(f"Erro ao pré-processar a imagem {img_path}: {e}")
@@ -30,7 +30,7 @@ def load_images(base_path: str) -> np.array:
                 if img is not None:
                     images.append(img)
         if not images:
-            print("Nenhuma imagem válida encontrada.")
+            raise ValueError("Nenhuma imagem válida encontrada.")
         return np.array(images)
     except Exception as e:
         print(f"Erro ao carregar imagens do diretório {base_path}: {e}")
@@ -53,22 +53,36 @@ def detect_faces(imgs: np.array) -> np.array:
         return np.array([])
 
 
-def cluster_images(features: np.array) -> np.array:
+def cluster_images(features: np.array, n_clusters: int) -> np.array:
     try:
         if len(features) == 0:
             raise ValueError("Nenhum recurso foi fornecido para clustering.")
         
-        clustering = DBSCAN(eps=0.5, min_samples=3).fit(features)
-        return clustering.labels_
+        kmeans = KMeans(n_clusters=n_clusters, random_state=42)
+        kmeans.fit(features)
+        return kmeans.labels_
     except Exception as e:
         print(f"Erro ao realizar clustering: {e}")
         return np.array([])
 
 
+def del_group_dir():
+    cluster_dir = os.path.abspath("cluster")
+    if os.path.exists(cluster_dir):
+        try:
+            shutil.rmtree(cluster_dir)
+            print(f"Diretório {cluster_dir} e seu conteúdo foram excluídos com sucesso.")
+        except Exception as e:
+            print(f"Erro ao excluir o diretório {cluster_dir}: {e}")
+    else:
+        print(f"O diretório {cluster_dir} não existe.")
+
+
 def make_groups_dir(cluster_dir, images, labels):
     try:
+        del_group_dir()
+        os.makedirs("cluster", exist_ok=True)
         unique_labels = set(labels)
-        print(f"Clusters identificados: {unique_labels}")
         
         for label in unique_labels:
             cluster_path = os.path.join(cluster_dir, f"cluster_{label}")
@@ -77,31 +91,27 @@ def make_groups_dir(cluster_dir, images, labels):
         for i, (img, label) in enumerate(zip(images, labels)):
             cluster_path = os.path.join(cluster_dir, f"cluster_{label}")
             img_path = os.path.join(cluster_path, f"image_{i}.jpg")
-            cv2.imwrite(img_path, (img * 255).astype(np.uint8))
+            cv2.imwrite(img_path, (img).astype(np.uint8))
         
         print("Imagens agrupadas e salvas com sucesso.")
     except Exception as e:
         print(f"Erro ao salvar as imagens nos diretórios: {e}")
 
 
-def group_faces(base_dir, cluster_dir):
+def group_faces(base_dir, cluster_dir, n_clusters):
     try:
-        print("Carregando imagens...")
         images = load_images(base_dir)
         if images.size == 0:
             raise ValueError("Nenhuma imagem foi carregada para o agrupamento.")
         
-        print("Detectando características faciais...")
         features = detect_faces(images)
         if features.size == 0:
             raise ValueError("Nenhuma característica foi extraída.")
         
-        print("Realizando agrupamento...")
-        labels = cluster_images(features)
+        labels = cluster_images(features, n_clusters)
         if labels.size == 0:
             raise ValueError("Nenhum agrupamento foi realizado.")
         
-        print("Criando diretórios de cluster...")
         make_groups_dir(cluster_dir, images, labels)
     except Exception as e:
         print(f"Erro no agrupamento de rostos: {e}")
@@ -109,5 +119,6 @@ def group_faces(base_dir, cluster_dir):
 
 if __name__ == "__main__":
     base_dir = 'C:/Users/eduar/Desktop/clustering de images/faces/'
-    cluster_dir = 'C:/Users/eduar/Desktop/clustering de images/cluster'
-    group_faces(base_dir, cluster_dir)
+    cluster_dir = os.path.abspath("cluster")
+    n_clusters = 10  
+    group_faces(base_dir, cluster_dir, n_clusters)
